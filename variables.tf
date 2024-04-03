789dcf79167c9caaf4644e20bb3f1f2f06851e82
Amish Thapa <amishthapamagar84@gmail.com>
2024-04-03 22:50:57 +0545

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "tf_backend_bucket_name" {
+  default = "lf-devops-gitops-terraform-state"
+}
+
+variable "devops_interns_repo_name" {
+  default = "gitops_demo_repo"
+}
